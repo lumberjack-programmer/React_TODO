ea7bc6f0bd2d8eddc053711905fc300047486d64
“lumberjack-programmer” <dmitry.p12@gmail.com>
2021-04-19 09:58:11 +0200

    react

---
+++ src/components/Tasks.js
@@ -0,0 +1,15 @@
+import Task from './Task'
+
+const Tasks = ( {tasks, onDelete, onToggle} ) => {
+
+  return (
+    <>
+      {tasks.map((task) =>
+       (<Task key={tasks.id} task={task}
+       onDelete={onDelete} onToggle={onToggle}
+       />))}
+    </>
+  )
+}
+
+export default Tasks
